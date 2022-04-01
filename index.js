cc510500b3a66dcc1e503e963c533f641d56d6de
Ayodejioladimeji <layobright11@gmail.com>
2022-04-01 12:15:42 +0100

    added more updates

--- index.js
+++ index.js
@@ -1,38 +1,49 @@
 // The Add commas function
 const addCommas = (num) => {
-  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
 
 // Remove Non Numeric function
 const removeNum = (num) => {
-  return num.toString().replace(/[^0-9]/g, '');
+  return num.toString().replace(/[^0-9]/g, "");
 };
 
 module.exports = {
   // The convert section
   convert: function (num) {
     if (Math.abs(num) > 999.999 && Math.abs(num) < 999999.999) {
-      return Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + 'k';
+      return Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + "k";
     } else if (Math.abs(num) > 999999.999 && Math.abs(num) < 999999999.999) {
-      return Math.sign(num) * (Math.abs(num) / 1000000).toFixed(1) + 'M';
+      return Math.sign(num) * (Math.abs(num) / 1000000).toFixed(1) + "M";
     } else if (
       Math.abs(num) > 999999999.999 &&
       Math.abs(num) < 999999999999.999
     ) {
-      return Math.sign(num) * (Math.abs(num) / 1000000000).toFixed(1) + 'B';
+      return Math.sign(num) * (Math.abs(num) / 1000000000).toFixed(1) + "B";
     } else if (Math.abs(num) > 999999999999.999) {
-      return Math.sign(num) * (Math.abs(num) / 1000000000000).toFixed(1) + 'T';
+      return Math.sign(num) * (Math.abs(num) / 1000000000000).toFixed(1) + "T";
     } else {
       return Math.sign(num) * Math.abs(num);
     }
   },
 
-  addComma: function (data) {
+  // The Strict add function
+  strictAddComma: function (data) {
     return addCommas(removeNum(data));
   },
 
-  // The Remove function
+  // The Add Comma function
+  addComma: function (num) {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  },
+
+  // The Remove Comma function
   removeComma: function (num) {
-    return num.toString().replace(/[^0-9]/g, '');
+    return parseInt(num.toString().replace(/\,/g, ""));
+  },
+
+  // The Strict Remove function
+  strictRemoveComma: function (num) {
+    return parseInt(num.toString().replace(/[^0-9]/g, ""));
   },
 };
--- test/script.js
+++
@@ -1,5 +0,0 @@
-const { convert, addComma, removeComma } = require('comma-separator');
-
-console.log(convert(5000000));
-console.log(addComma(5000000));
-console.log(removeComma('5,000,000'));
